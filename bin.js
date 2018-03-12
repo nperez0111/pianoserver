25938f79849bd0b2fb9c8eb0da692ccb40a8cef1
Nick Perez <nperez0111@gmail.com>
2018-03-12 01:45:40 -0700

    quit server and proccess on ESC

--- bin.js
+++ bin.js
@@ -136,6 +136,8 @@ function connectToConsole() {
             }
             if (key === '\u001B') {
                 quitServer()
+                console.log('Server has been stopped.')
+                process.exit()
             }
 
             //send single char and flush stdin
