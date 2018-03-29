7efb069ac6c755cb1020f9520b003be560109b59
Nick Perez <nperez0111@gmail.com>
2018-03-28 17:10:11 -0700

    add a launcher

---
+++ launcher.js
@@ -0,0 +1,53 @@
+const serverCommands = require('./serverCommands'),
+    startServer = serverCommands.startServer,
+    quitServer = serverCommands.quitServer,
+    restartServer = serverCommands.restartServer,
+    checkIfRunning = serverCommands.checkIfRunning,
+    express = require('express'),
+    app = express()
+
+app.get('/startServer', (req, res) => {
+    checkIfRunning().then(() => {
+        res.send("running")
+    }).catch(() => {
+        const subdomain = 'pianoserver',
+            port = 8081
+
+        startServer(subdomain, port).then(() => {
+            res.send('started')
+        }).catch(() => {
+            res.send('failed')
+        })
+    })
+})
+app.get('/restartServer', (req, res) => {
+    checkIfRunning().then(() => {
+        restartServer().then(() => {
+            res.send('restarting')
+        }).catch(() => {
+            res.send('failed')
+        })
+    }).catch(() => {
+        res.send('not running')
+    })
+})
+app.get('/quitServer', (req, res) => {
+    checkIfRunning().then(() => {
+        quitServer().then(() => {
+            res.send('quitted')
+        }).catch(() => {
+            res.send('failed')
+        })
+    }).catch(() => {
+        res.send('not running')
+    })
+})
+app.get('/serverStatus', (req, res) => {
+    checkIfRunning().then(() => {
+        res.send('running')
+    }).catch(() => {
+        res.send('not running')
+    })
+})
+
+app.listen(8082)
